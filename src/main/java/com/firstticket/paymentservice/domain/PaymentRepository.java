0f59ab1e1f9b4017412c9bf9561dde520e3b3876
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-23 12:27:56 +0900

    feat: PaymentRepository 작성 (PaymentHistoryRepository는 특정 결제의 이력 혹은 특정 상태의 이력 조회할 때 생성)

---
+++ src/main/java/com/firstticket/paymentservice/domain/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.firstticket.paymentservice.domain;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PaymentRepository {
+
+    Payment save(Payment payment);
+
+    Optional<Payment> findById(UUID id);
+
+    Optional<Payment> findByOrderId(String orderId);
+}
