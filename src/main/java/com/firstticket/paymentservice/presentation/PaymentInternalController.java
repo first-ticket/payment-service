8697fed12f1f27f383076860fc428d83b5042a69
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-29 10:10:24 +0900

    feat: PaymentInternalController @ConditionalOnProperty 추가

--- src/main/java/com/firstticket/paymentservice/presentation/PaymentInternalController.java
+++ src/main/java/com/firstticket/paymentservice/presentation/PaymentInternalController.java
@@ -7,12 +7,18 @@ import com.firstticket.paymentservice.presentation.dto.request.PaymentCreateRequ
 import com.firstticket.paymentservice.presentation.dto.response.PaymentResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ConditionalOnProperty(
+    prefix = "feature.internal-payments",
+    name = "enabled",
+    havingValue = "true"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/internal/v1/payments")
